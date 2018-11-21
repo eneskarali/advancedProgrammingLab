3d75f2cb3dba55b3218d5982862ed3c0238d9d3c
Unknown <eneskarali96@hotmail.com>
2018-11-21 17:47:48 +0300

    CW7- PWA manifest fixed

--- PWA/sw.js
+++ PWA/sw.js
@@ -1,24 +1,27 @@
-
 const CACHE ='JS'
+const FILES = ['https://github.com/eneskarali/advancedProgrammingLab', '/advancedProgrammingLab/HW3/animations.html', '/JS/index.html']
 function installCB(e) {
-  console.log('install oldu', e.request);
+  e.waitUntil(
+    caches.open(CACHE)
+    .then(cache => cache.addAll(FILES))
+    .catch(console.log)
+  )
 }
 self.addEventListener('install', installCB)
 
 function save(req, resp) {
-  return caches.open(CACHE)
-  .then(cache => {
-    cache.put(req, resp.clone());
-    return resp;
-  })
-  .catch(console.log)
-}
-function fetchCB(e) { //fetch first
-  let req = e.request
-  console.log('JS', req.url);
-  e.respondWith(
-    fetch(req).then(r2 => save(req, r2))
-    .catch(() => { return caches.match(req).then(r1 => r1) })
-  )
-}
-self.addEventListener('fetch', fetchCB)
+    return caches.open(CACHE)
+    .then(cache => {
+      cache.put(req, resp.clone());
+      return resp;
+    }) 
+    .catch(console.log)
+  }
+  function fetchCB(e) { //fetch first
+    let req = e.request
+    e.respondWith(
+      fetch(req).then(r2 => save(req, r2))
+      .catch(() => { return caches.match(req).then(r1 => r1) })
+    )
+  }
+  self.addEventListener('fetch', fetchCB)
