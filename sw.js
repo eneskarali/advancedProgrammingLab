d8ec7bc14630c143e7a708116e640afce1f089f6
Unknown <eneskarali96@hotmail.com>
2018-11-28 15:20:19 +0300

    CW7 - Cache links updated

--- sw.js
+++ sw.js
@@ -1,6 +1,10 @@
-const CACHE ='JS'
-const FILES = ['/advancedProgrammingLab/', '/advancedProgrammingLab/CW7/', '/advancedProgrammingLab/CW7/documentObjectModel.html'
-,'/advancedProgrammingLab/HW3/','/advancedProgrammingLab/HW3/animations.html']
+const CACHE ='ns'
+const FILES = ['/advancedProgrammingLab/CW2/ArrayExamples.html', '/advancedProgrammingLab/CW3/inspector.html', 
+'/advancedProgrammingLab/CW4/index.html', '/advancedProgrammingLab/CW5/selectFile.html', '/advancedProgrammingLab/CW6/performanceTest.html'
+,'/advancedProgrammingLab/CW7/documentObjectModel.html', '/advancedProgrammingLab/HW1/HW1courseData.html', 
+'/advancedProgrammingLab/HW2/index.html',
+'/advancedProgrammingLab/HW3/animations.html']
+
 function installCB(e) {
   e.waitUntil(
     caches.open(CACHE)
@@ -10,26 +14,17 @@ function installCB(e) {
 }
 self.addEventListener('install', installCB)
 
-function cacheCB(e) { //cache first
-  let req = e.request
-  e.respondWith(
-    caches.match(req)
-    .then(r1 => r1 || fetch(req))
-    .catch(console.log)
-  )
-}
-self.addEventListener('fetch', cacheCB)
-
 function save(req, resp) {
   return caches.open(CACHE)
   .then(cache => {
     cache.put(req, resp.clone());
     return resp;
-  }) 
+  })
   .catch(console.log)
 }
 function fetchCB(e) { //fetch first
   let req = e.request
+  console.log('ns', req.url);
   e.respondWith(
     fetch(req).then(r2 => save(req, r2))
     .catch(() => { return caches.match(req).then(r1 => r1) })
